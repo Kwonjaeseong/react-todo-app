018c11eae7ce921aecaa98f56c80cd864b0a456e
Kwonjaeseong <77simon@naver.com>
2024-06-13 11:49:18 +0900

    init

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,26 @@
+import { useState } from "react";
+
 export default function Header() {
+  const [modal, setModal] = useState(false);
+
   return (
-    <div className="header">
-      <h1>🎃 Todo</h1>
-      <button>정보</button>
-    </div>
+    <>
+      <div className="header">
+        <h1>🎃 Todo</h1>
+        <button onClick={() => setModal(!modal)}>정보</button>
+      </div>
+
+      <div className={modal ? "modal show" : "modal"}>
+        <div className="inner">
+          <p>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+            Dignissimos culpa nesciunt quidem voluptate enim accusantium magni
+            distinctio mollitia ipsum fugiat, laudantium eius voluptas molestiae
+            temporibus unde voluptates maxime? Odio, accusamus?
+          </p>
+          <button onClick={() => setModal(!modal)}>닫기</button>
+        </div>
+      </div>
+    </>
   );
 }
